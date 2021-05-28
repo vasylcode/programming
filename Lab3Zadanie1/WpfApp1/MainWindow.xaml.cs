b5dc097b414b1446d8e22feee97bc2e74712d699
vasylcode <vasylcode@gmail.com>
2021-05-28 08:10:40 +0200

    lab3

---
+++ Lab3Zadanie1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        double a, b;
+        string result;
+
+        private void btnRoznica_Click(object sender, RoutedEventArgs e) {
+            a = double.Parse(txtboxA.Text);
+            b = double.Parse(txtboxB.Text);
+            result = (a - b).ToString();
+            txtboxWynik.Text = "Różnica: " + a + " - " + b + " = " + result;
+        }
+
+        private void btnOczyn_Click(object sender, RoutedEventArgs e) {
+            try { 
+                a = double.Parse(txtboxA.Text);
+                b = double.Parse(txtboxB.Text);
+                if (b == 0) {
+                    throw new Exception("Nie można dzielić przez 0!");
+                }
+                result = (a / b).ToString();
+                txtboxWynik.Text = "Iloczyn: " + a + " / " + b + " = " + result;
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK);
+            }
+        }
+
+        private void btnOraz_Click(object sender, RoutedEventArgs e) {
+            a = double.Parse(txtboxA.Text);
+            b = double.Parse(txtboxB.Text);
+            result = (a * b).ToString();
+            txtboxWynik.Text = "Iloraz: " + a + " * " + b + " = " + result;
+        }
+
+        private void btnSuma_Click(object sender, RoutedEventArgs e) {
+            a = double.Parse(txtboxA.Text);
+            b = double.Parse(txtboxB.Text);
+            result = (a + b).ToString();
+            txtboxWynik.Text = "Suma: " + a + " + " + b + " = " + result;
+        }
+    }
+}
---
+++ Lab3Zadanie2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3Zadanie2 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        double coef, val, result;
+
+        private void btnWylicz_Click(object sender, RoutedEventArgs e) {
+            try {
+                if ((listbox.SelectedItem as ListBoxItem).Content.ToString() == "Rzeszów") {
+                    coef = 0.6;
+                } else if ((listbox.SelectedItem as ListBoxItem).Content.ToString() == "Warszawa") {
+                    coef = 0.8;
+                } else {
+                    coef = 0.5;
+                }
+
+                if (rdrNormalny.IsChecked == true) {
+                    val = 6;
+                } else if (rdrUstawowy.IsChecked == true) {
+                    val = 3;
+                } else if (rdrSamorzad.IsChecked == true) {
+                    val = 4;
+                }
+                result = val * coef;
+                txtResult.Text = Math.Round(result, 2).ToString();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OK);
+            }
+        }
+    }
+}
