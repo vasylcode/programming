d1ac658753dba0a4d1734a0fe19520ba42623360
vasylcode <vasylcode@gmail.com>
2021-06-05 15:19:06 +0200

    Lab4

---
+++ Lab4/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public List<Student> ListaStudentow { get; set; }
+
+        public MainWindow() {
+            ListaStudentow = new List<Student>() {
+                new Student() { imie = "Jan", nazwisko = "Kowalski", NrIndeksu = 1243, wydzial="KIS" }
+            };
+
+            InitializeComponent();
+
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Imię", Binding = new Binding("imie") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Nazwisko", Binding = new Binding("nazwisko") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Nr indeksu", Binding = new Binding("NrIndeksu") });
+            dgStudent.Columns.Add(new DataGridTextColumn() { Header = "Wydzial", Binding = new Binding("wydzial") });
+
+            dgStudent.AutoGenerateColumns = false;
+            dgStudent.ItemsSource = ListaStudentow;
+
+        }
+
+        private void bAdd_Student(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SEdit();
+            if (dialog.ShowDialog() == true) {
+                ListaStudentow.Add(dialog.student);
+                dgStudent.Items.Refresh();
+            }
+        }
+
+        private void bRemove_Student(object sender, RoutedEventArgs e)
+        {
+            if (dgStudent.SelectedItem is Student) {
+                ListaStudentow.Remove((Student)dgStudent.SelectedItem);
+                dgStudent.Items.Refresh();
+            }
+        }
+
+        private void bEdit_Student(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Lab4/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Student
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int NrIndeksu { get; set; }
+        public string wydzial { get; set; }
+
+
+        public Student(string imie, string nazwisko, int NrIndeksu, string wydzial)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.NrIndeksu = NrIndeksu;
+            this.wydzial = wydzial;
+        }
+
+        public Student()
+            : this("", "", 0, "")
+        { }
+    }
+}
